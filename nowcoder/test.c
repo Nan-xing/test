84accbe07a9d409e9176e8953e9d3d23bf016aa8
Nan-xing <135837296+Nan-xing@users.noreply.github.com>
2023-08-11 16:05:26 +0800

    创建 b13bdd86-a30b-4c8a-9922-86d4d4601ab3.vsidx

---
+++ nowcoder/test.c
@@ -0,0 +1,26 @@
+
+
+#include "stdio.h"
+
+
+////2的n次方的计算
+//    int main()
+//    {
+//        int n = 0;
+//        int ret = 0;
+//        scanf("%d", &n);
+//        ret = 2 << (n - 1);
+//        printf("%d", ret);
+//        return 0;
+//    }
+
+////你能活多少秒
+//int main() {
+//    int age = 0;
+//    long long  year_s = 3156 * (10 * 10 * 10 * 10);
+//    scanf("%d", &age);
+//    printf("%lld", age * year_s);
+//    return 0;
+//}
+
+
