054af214feb4e2bc16d4c3c28121b64a22b35665
Nan-xing <135837296+Nan-xing@users.noreply.github.com>
2023-08-13 16:18:50 +0800

    更新 test.c

--- nowcoder/test.c
+++ nowcoder/test.c
@@ -87,4 +87,77 @@
 //}
 
 
+////N个数字之和
+//int main() {
+//    int n = 0, i = 0, j = 0;
+//    int sum = 0;
+//    int arr[] = { 0 };
+//    scanf("%d", &n);
+//    for (i = 0; i < n; i++)
+//    {
+//        scanf("%d", &j);
+//        arr[i] = j;
+//        sum = sum + arr[i];
+//    }
+//
+//    printf("%d", sum);
+//    return 0;
+//}
 
+
+////计算N个数最大值与最小值的差
+//int main() {
+//    int arr[20] = { 0 };
+//    int n = 0;
+//    int max = 0, min = 0, temp = 0;
+//    int i = 0;
+//    scanf("%d", &n);
+//    for (i = 0; i < n; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    for (i = 0; i < n; i++)
+//    {
+//        if (arr[i] >= max)
+//        {
+//            max = arr[i];
+//        }
+//    }
+//    min = max;
+//    for (i = 0; i < n; i++)
+//    {
+//        if (arr[i] <= min)
+//        {
+//            min = arr[i];
+//        }
+//    }
+//    printf("%d", max - min);
+//    return 0;
+//}
+
+
+
+//冒泡排序
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,9,7,8,9,10 };
+	int i = 0, j = 0;
+	for (i = 0; i < 9; i++)
+	{
+		int temp = 0;
+		for (j = 0; j < 9-i; j++)
+		{
+			if (arr[j] <arr[j + 1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j+ 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
