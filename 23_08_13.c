9e6593771ec1e1086d0d94890d74c3490d548130
Nan-xing <135837296+Nan-xing@users.noreply.github.com>
2023-08-14 13:13:01 +0800

    创建 23_08_13.c

---
+++ 23_08_13.c
@@ -0,0 +1,49 @@
+#include "stdio.h"
+
+//二维数组的输入输出
+//int main()
+//{
+//    float arr[5][6] = { 0.0 };
+//    int i = 0, j = 0;
+//
+//    for (i = 0; i < 5; i++)
+//    {
+//        float sum = 0.0;
+//        for (j = 0; j < 5; j++)
+//        {
+//            scanf("%f", &arr[i][j]);
+//            sum += arr[i][j];
+//        }
+//        arr[i][5] = sum;
+//    }
+//    for (i = 0; i < 5; i++)
+//    {
+//        for (j = 0; j < 6; j++)
+//        {
+//            printf("%.1f ", arr[i][j]);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+
+
+//指定输出对应位置的数据
+//int main()
+//{
+//    int arr[20][20] = { 0 };
+//    int n, m, x, y;
+//    scanf("%d%d", &n, &m);
+//    for (x = 0; x < n; x++)
+//    {
+//        for (y = 0; y < m; y++)
+//        {
+//            scanf("%d", &arr[x][y]);
+//        }
+//    }
+//    scanf("%d%d", &x, &y);
+//    printf("%d", arr[x - 1][y - 1]);
+//
+//    return 0;
+//}
+
