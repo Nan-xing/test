25dce96ed4c62a570f1048a6dd40200abcd4f90f
Nan-xing <135837296+Nan-xing@users.noreply.github.com>
2023-08-25 13:44:03 +0800

    Create 23_08_24.c

---
+++ 23_08_24.c
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+////≈–∂œ «‘™“Ùªπ «∏®“Ù
+//int Jude(char i)
+//{
+//    char arr[] = "aeiouAEIOU";
+//    int j;
+//    int len = strlen(arr);
+//    for (j = 0; j < len; j++)
+//    {
+//        if (i == arr[j])
+//            return 1;
+//    }
+//    return 0;
+//}
+//
+//int main() {
+//    char i;
+//    while (scanf("%c", &i) != EOF)
+//    {
+//        getchar();
+//        int ret = Jude(i);
+//        if (ret)
+//            printf("Vowel\n");
+//        else
+//            printf("Consonant\n");
+//    }
+//    return 0;
+//}
+
+//
+////≈–∂œ◊÷ƒ∏
+//int JudeCh(char n)
+//{
+//    if (n >= 'A' && n <= 'z')
+//        return 1;
+//    else
+//        return 0;
+//}
+//int main() {
+//    char n;
+//    scanf("%c", &n);
+//    int ret = JudeCh(n);
+//    if (ret)
+//        printf("YES\n");
+//    else
+//        printf("NO\n");
+//    return 0;
+//}
+
+
+////(BMI)Ω°øµ∆¿π¿
+//int main() {
+//    float weight, height;
+//    scanf("%f%f", &weight, &height);
+//    float BMI = 0;
+//    BMI = weight / pow(height, 2);
+//    if (BMI >= 18.5 && BMI <= 23.9)
+//        printf("Normal\n");
+//    else
+//        printf("Abnormal\n");
+//    return 0;
+//}
+
+
+////≈£≈£µƒ∂˛»˝∆ﬂ’˚≥˝
+//int main() {
+//    int num = 0;
+//    scanf("%d", &num);
+//    int count = 0;
+//    int arr[] = { 2,3,7 };
+//    int i = 0;
+//    for (i = 0; i < 3; i++)
+//    {
+//        if (num % arr[i] == 0)
+//        {
+//            printf("%d ", arr[i]);
+//            count++;
+//        }
+//    }
+//    if (count == 0)
+//    {
+//        printf("n");
+//    }
+//    return 0;
+//}
